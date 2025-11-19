a435b0469033a2891623c0e4a15b9b5a5382b1e1
Freitassync <gabrielfreitas3010@outlook.com>
2025-11-19 07:06:37 -0300

    feat: initial implementation of minimalist calculator

---
+++ script.js
@@ -0,0 +1,159 @@
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    clear() {
+        this.currentOperand = '';
+        this.previousOperand = '';
+        this.operation = undefined;
+    }
+
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
+
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
+
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return;
+        if (this.previousOperand !== '') {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    compute() {
+        let computation;
+        const prev = parseFloat(this.previousOperand);
+        const current = parseFloat(this.currentOperand);
+        if (isNaN(prev) || isNaN(current)) return;
+        switch (this.operation) {
+            case '+':
+                computation = prev + current;
+                break;
+            case '-':
+                computation = prev - current;
+                break;
+            case '*':
+                computation = prev * current;
+                break;
+            case '÷':
+                if (current === 0) {
+                    alert("Não é possível dividir por zero!");
+                    this.clear();
+                    return;
+                }
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = '';
+    }
+
+    getDisplayNumber(number) {
+        const stringNumber = number.toString();
+        const integerDigits = parseFloat(stringNumber.split('.')[0]);
+        const decimalDigits = stringNumber.split('.')[1];
+        let integerDisplay;
+        if (isNaN(integerDigits)) {
+            integerDisplay = '';
+        } else {
+            integerDisplay = integerDigits.toLocaleString('pt-BR', { maximumFractionDigits: 0 });
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay},${decimalDigits}`;
+        } else {
+            return integerDisplay;
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText = this.getDisplayNumber(this.currentOperand);
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText = 
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`;
+        } else {
+            this.previousOperandTextElement.innerText = '';
+        }
+    }
+}
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operator]');
+const equalsButton = document.querySelector('[data-action="calculate"]');
+const deleteButton = document.querySelector('[data-action="delete"]');
+const allClearButton = document.querySelector('[data-action="clear"]');
+const previousOperandTextElement = document.getElementById('previous-operand');
+const currentOperandTextElement = document.getElementById('current-operand');
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.getAttribute('data-number'));
+        calculator.updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.getAttribute('data-operator'));
+        calculator.updateDisplay();
+    });
+});
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute();
+    calculator.updateDisplay();
+});
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear();
+    calculator.updateDisplay();
+});
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete();
+    calculator.updateDisplay();
+});
+
+// Keyboard support
+document.addEventListener('keydown', (e) => {
+    if ((e.key >= 0 && e.key <= 9) || e.key === '.') {
+        calculator.appendNumber(e.key);
+        calculator.updateDisplay();
+    }
+    if (e.key === '+' || e.key === '-' || e.key === '*') {
+        calculator.chooseOperation(e.key);
+        calculator.updateDisplay();
+    }
+    if (e.key === '/') {
+        e.preventDefault(); // Prevent quick find in Firefox
+        calculator.chooseOperation('÷');
+        calculator.updateDisplay();
+    }
+    if (e.key === 'Enter' || e.key === '=') {
+        e.preventDefault();
+        calculator.compute();
+        calculator.updateDisplay();
+    }
+    if (e.key === 'Backspace') {
+        calculator.delete();
+        calculator.updateDisplay();
+    }
+    if (e.key === 'Escape') {
+        calculator.clear();
+        calculator.updateDisplay();
+    }
+});
